635767bc66d9737223143e89cf1bf0b73794c290
coded hola <codedhola2000@gmail.com>
2022-11-12 17:54:14 +0100

    feat: completed user registration route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,29 @@ router.get("/", (req, res) => {
   res.send("welcome to the project")
 })
 
+router.post("/register", async (req, res) => {
+  let {name, email, password} = req.body
+  try {
+    const validateUser = await User.findOne({email: req.body.email})
+    console.log(validateUser)
+    if(validateUser) return res.status(400).json({status: "Failed", message: "User already exist"})
+
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, password})
+
+    res.status(201).json({
+      status: "Successful",
+      user: {user: user.name, email: user.email}
+    })
+  }catch(err){
+    res.status(400).json({
+      status: "Fail",
+      message: err.message
+    })
+  }
+})
 
 
 module.exports = router
