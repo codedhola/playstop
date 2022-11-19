62ed44573e09cc9c5ce5db9155c587bd60e5c6c3
coded hola <codedhola2000@gmail.com>
2022-11-19 16:11:39 +0100

    feat: added auth middleware functionality

--- server/utils/authMiddleware.js
+++ server/utils/authMiddleware.js
@@ -1,7 +1,30 @@
 const jwt = require("jsonwebtoken")
 
-const authenticated = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET)
+const authenticated = async (req, res, next) => {
+    let token;
+
+    // SEARCH FOR TOKEN FROM REQUEST
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        token = req.headers.authorization.split(" ")[1]; // GET THE TOKEN STRING
+    }
+    //  CHECK IF TOKEN IS AVAILABLE
+    if(!token) return next("An Error Occured");
+    try {
+        // CHECK IF JWT TOKEN IS VALID 
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+        
+        // CHECK IF JWT TOKEN HAS A VALID USER FROM DATABASE
+        const user = await User.findById(decoded.id);
+
+        
+    if(!user) return next("User Not found"); // ERROR IF INVALID USER
+
+    req.user = user;
+
+    next();
+    }catch(err){
+        next("An Error occured")
+    }
 }
 
 module.exports = { authenticated }
